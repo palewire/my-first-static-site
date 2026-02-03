75d47b8634c4f7c0410ce289f14bb08b706cf741
Ben <Ben.Welsh@thomsonreuters.com>
2026-02-02 20:44:14 -0500

    Update article metadata in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
   import RelatedLinks from '$lib/components/RelatedLinks.svelte';
 
   // Article metadata
-  let headline = 'Become a force for good. Join our next class.';
-  let byline = 'NYCity News Service';
-  let pubDate = '2026-01-31';
+  let headline = 'Go CUNY!';
+  let byline = 'Tiana McGee';
+  let pubDate = '2026-02-02';
 
   // Related stories
   const relatedStories = [
